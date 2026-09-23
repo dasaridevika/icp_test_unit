3d50f711be865ed7a67199901f1d55f979b8639a
Devika <telanganprince1826@gmail.com>
2026-09-23 13:18:03 +0530

    Fix circular import deadlock in engine package

--- engine/__init__.py
+++ engine/__init__.py
@@ -3,34 +3,3 @@ Jev ICP Revenue Intelligence Studio Engine
 Powered by TypeSafe AI / Jev System One Decision Primitives
 """
 
-from engine.client import JevClient, JevResponse
-from engine.gtm_engine import (
-    GTMScoringEngine,
-    CompanyStandardsConfig,
-    StreamlinedLeadForm,
-    StreamlinedScoringResult
-)
-from engine.primitives import (
-    ICP_NOUL_QUESTIONS,
-    ICP_CHOICE_QUESTIONS,
-    ICP_SCORE_QUESTIONS,
-    build_lead_state
-)
-from engine.fx import FXEngine
-from engine.rules import PolicyEngine, PolicyCheckResult
-
-__all__ = [
-    "JevClient",
-    "JevResponse",
-    "GTMScoringEngine",
-    "CompanyStandardsConfig",
-    "StreamlinedLeadForm",
-    "StreamlinedScoringResult",
-    "ICP_NOUL_QUESTIONS",
-    "ICP_CHOICE_QUESTIONS",
-    "ICP_SCORE_QUESTIONS",
-    "build_lead_state",
-    "FXEngine",
-    "PolicyEngine",
-    "PolicyCheckResult"
-]
